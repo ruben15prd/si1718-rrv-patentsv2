2efac15d34b7d9c0feb22400734290cca8d1d80f
Ruben Ramirez Vera <ruben15prd@gmail.com>
2017-11-28 08:32:24 +0000

    Managing errors in UI

--- public/create-ctrl.js
+++ public/create-ctrl.js
@@ -23,12 +23,21 @@ angular.module("PatentManagerApp")
                 }, function(error){
                     console.log(error.status);
                     
-                    if(error.status == '422'){
-                    $scope.error = "Please review the information entered in the fields";
-                    }
-                    if(error.status == '409'){
-                    $scope.error = "There is another patent with same name and date";
-                    }
+                    if(error.status != '200'){
+                    switch (error.status) {
+                        case '422':
+                            $scope.error = "Please review the information entered in the fields";
+                            break;
+                        case '409':
+                            $scope.error = "There is another patent with same name and date";
+                            break;
+                        default:
+                            $scope.error = "Error, please contact administrator";
+                            break;
+}
+                    
+                    }  
+                  
                     //alert(error.data);
                 });
             
